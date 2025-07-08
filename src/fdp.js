313f9747944d02b2d42144078bf94b4d937e4f98
itochs <halglobe0108@gmail.com>
2025-07-08 22:08:50 +0900

    impl fdp

--- src/fdp.js
+++ src/fdp.js
@@ -5,18 +5,33 @@ class Node {
   }
 }
 
-const quotientForce = (alpha, beta, weight) => {
+const quotientForce = (weight, alpha, beta) => {
   function force(node_pairs, distance_matrix) {
     // 単位ベクトルe_{ij}はノードペアをつないだ方向xi->xj
-    function force_1d(xi, xj, dist) {
-      let dx = max(0.01, Math.hypot(xi, xj));
-      let ex = (xj - xi) / dx;
-      return (weight * Math.pow(dx, alpha) * ex) / Math.pow(dist, beta);
+    function force_1d(xi, xj, actual_distance, graph_distance) {
+      let ex = (xj - xi) / actual_distance;
+      return (
+        (weight * Math.pow(actual_distance, alpha) * ex) /
+        Math.pow(graph_distance, beta)
+      );
     }
-    let f = [0, 0];
+    let f = new Array(distance_matrix.length).fill(0).map(() => [0, 0]);
     for (let [xi, xj] of node_pairs) {
-      f[0] += force_1d(xi.x, xj.x, distance_matrix[xi.index][xj.index]);
-      f[1] += force_1d(xi.y, xj.y, distance_matrix[xi.index][xj.index]);
+      const dx = xj.p.x - xi.p.x;
+      const dy = xj.p.y - xi.p.y;
+      const d = max(0.001, Math.hypot(dx, dy));
+      f[xi.index][0] += force_1d(
+        xi.p.x,
+        xj.p.x,
+        d,
+        distance_matrix[xi.index][xj.index]
+      );
+      f[xi.index][1] += force_1d(
+        xi.p.y,
+        xj.p.y,
+        d,
+        distance_matrix[xi.index][xj.index]
+      );
     }
 
     return f;
@@ -25,12 +40,29 @@ const quotientForce = (alpha, beta, weight) => {
   return force;
 };
 
-const calc_distance_matrix = (nodes) => {
+const calc_distance_matrix = (nodes, edgeIndices) => {
   let n = nodes.length;
-  let distance_matrix = new Array(n).fill(0).map((_) => new Array(n).fill(-1));
+  let distance_matrix = new Array(n).fill(0).map((_) => new Array(n).fill(0));
+  for (let i = 0; i < n; i++) {
+    distance_matrix[i][i] = 0;
+  }
+  for ([i, j] of edgeIndices) {
+    distance_matrix[i][j] = 1;
+    distance_matrix[j][i] = 1;
+  }
+  const edgeIndicesSet = new Set(edgeIndices);
   for (i of nodes) {
     for (j of nodes) {
+      if (!edgeIndicesSet.has([i.index, j.index])) {
+        continue;
+      }
       for (k of nodes) {
+        if (
+          !edgeIndicesSet.has([i.index, k.index]) ||
+          !edgeIndicesSet.has([k.index, j.index])
+        ) {
+          continue;
+        }
         distance_matrix[i.index][j.index] = min(
           distance_matrix[i.index][j.index],
           distance_matrix[i.index][k.index] + distance_matrix[k.index][j.index]
@@ -38,18 +70,72 @@ const calc_distance_matrix = (nodes) => {
       }
     }
   }
+  return distance_matrix;
 };
 
 // layout and view?
 class FDPLayout {
-  constructor(nodes, edges) {
+  constructor(nodes, edgeIndices) {
     this.nodes = nodes;
-    this.edges = edges;
+    this.edgeIndices = edgeIndices;
     this.tree = new BarnesHutTree(new Boundary(0, 0, width, height));
     this.renderer = new RendererBHT(this.tree);
     let n = nodes.length;
-    this.distance_matrix = calc_distance_matrix(nodes);
+    this.distance_matrix = calc_distance_matrix(nodes, this.edgeIndices);
   }
 
-  update() {}
+  update(temperature) {
+    const applyAttractiveForce = quotientForce(1, 2, 0);
+    const attractivePairs = this.edgeIndices
+      .map(([i, j]) => [
+        [this.nodes[i], this.nodes[j]],
+        [this.nodes[j], this.nodes[i]],
+      ])
+      .flat();
+    const attractiveForce = applyAttractiveForce(
+      attractivePairs,
+      this.distance_matrix
+    );
+
+    const applyRepulsiveForce = quotientForce(-1, -1, 0);
+    const repulsivePairs = this.nodes
+      .map((v) => this.nodes.map((u) => [v, u]))
+      .flat();
+    const repulsiveForce = applyRepulsiveForce(
+      repulsivePairs,
+      this.distance_matrix
+    );
+
+    const sumEachForce = attractiveForce.map((_, i) => {
+      const sumForce = [
+        attractiveForce[i][0] + repulsiveForce[i][0],
+        attractiveForce[i][1] + repulsiveForce[i][1],
+      ];
+      return sumForce;
+    });
+    let n = this.nodes.length;
+    this.nodes = this.nodes.map((v) => {
+      const clamp = (x, l, r) => min(r, max(l, x));
+      v.p.x = clamp(
+        v.p.x + sumEachForce[v.index][0] * temperature,
+        0,
+        Math.sqrt(n)
+      );
+      v.p.y = clamp(
+        v.p.y + sumEachForce[v.index][1] * temperature,
+        0,
+        Math.sqrt(n)
+      );
+      return v;
+    });
+  }
+
+  start(iter = 100) {
+    const maxTemperature = 1;
+    let temperature = maxTemperature;
+    for (let t = 0; t < iter; t += 1) {
+      this.update(temperature);
+      temperature = (maxTemperature / iter) * (iter - t - 1);
+    }
+  }
 }
--- src/sketch.js
+++ src/sketch.js
@@ -1,18 +1,69 @@
-// let tree = null;
-// let renderer = null;
+let tree = null;
+let renderer = null;
+let temperature = 1;
+let iter = 10;
+let t = 0;
+let layout = null;
 
 function setup() {
   createCanvas(400, 400);
-  //   tree = new BarnesHutTree(new Boundary(0, 0, width, height));
-  //   for (let i = 0; i < 10; i++) {
-  //     tree.add(new Point(Math.random() * width, Math.random() * height, 1));
-  //   }
-  //   renderer = new RendererBHT(tree);
-  let layout = new FDPLayout([new Node(0, 0, 0), new Node(0, 0, 1)], [[0, 1]]);
+  // tree = new BarnesHutTree(new Boundary(0, 0, width, height));
+  // for (let i = 0; i < 10; i++) {
+  //   tree.add(new Point(Math.random() * width, Math.random() * height, 1));
+  // }
+  // renderer = new RendererBHT(tree);
+
+  const graph = [[1], [0, 3, 4], [3, 5], [1, 2, 4], [1, 3], [2]];
+  const pos = [
+    [0.6897927410102785, 1.8633360657738784],
+    [1.998349565808713, 2.148706089170453],
+    [0.5146614361273951, 0.9543305881316088],
+    [1.2990912822525318, 1.5576078551919947],
+    [1.2641901566595386, 1.8782536273884058],
+    [0.11634882392203322, 1.890762445560767],
+  ];
+
+  const nodes = pos.map(([x, y], i) => {
+    let n = new Node(x, y, i);
+    return n;
+  });
+  const edgeIndices = graph.map((vs, i) => vs.map((v) => [i, v])).flat();
+
+  layout = new FDPLayout(nodes, edgeIndices);
+  frameRate(10);
 }
 
 function draw() {
   background(255);
-  //   renderer.draw();
-  //   noLoop();
+  // renderer.draw();
+  layout.update(temperature);
+  let n = layout.nodes.length;
+
+  const scale = (x, y) => {
+    return [(x / Math.sqrt(n)) * width, height - (y / Math.sqrt(n)) * height];
+  };
+  layout.edgeIndices.forEach(([i, j]) => {
+    let { x: xi, y: yi } = layout.nodes[i].p;
+    let { x: xj, y: yj } = layout.nodes[j].p;
+    let [scaledXi, scaledYi] = scale(xi, yi);
+    let [scaledXj, scaledYj] = scale(xj, yj);
+    stroke(0);
+    strokeWeight(3);
+    line(scaledXi, scaledYi, scaledXj, scaledYj);
+  });
+
+  layout.nodes.forEach((v) => {
+    let { x, y } = v.p;
+    let [scaledX, scaledY] = scale(x, y);
+    noStroke();
+    fill(0, 0, 255);
+    ellipse(scaledX, scaledY, 10, 10);
+  });
+
+  temperature *= 0.9;
+  t += 1;
+  if (temperature < 0.0001) {
+    noLoop();
+  }
+  console.log("temp", temperature, "iter t", t);
 }
