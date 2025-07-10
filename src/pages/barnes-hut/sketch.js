522772447488372fa8593967a70f2d3b8eaf5eda
itochs <halglobe0108@gmail.com>
2025-07-10 12:09:52 +0900

    初期化

--- src/pages/barnes-hut/sketch.js
+++ src/pages/barnes-hut/sketch.js
@@ -1,12 +1,16 @@
 let tree = null;
+let renderer = null;
 
 function setup() {
   createCanvas(400, 400);
-  tree = new BarnesHutTree();
+  tree = new BarnesHutTree(new Boundary(0, 0, width, height));
+  for (let i = 0; i < 100; i++) {
+    tree.add(new Point(Math.random() * width, Math.random() * height));
+  }
+  renderer = new RendererBHT(tree);
 }
 
 function draw() {
   background(255);
-  fill(255, 0, 0);
-  ellipse(100, 100, 100, 100);
+  renderer.draw();
 }
