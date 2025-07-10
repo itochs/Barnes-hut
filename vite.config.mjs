db3e6cfd72a6ba236a2db753487231afae4f33c2
itochs <halglobe0108@gmail.com>
2025-07-10 15:04:34 +0900

    rollupが必要かわかってない

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,15 +1,21 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        nested: resolve(__dirname, 'src/pages/barnes-hut/index.html'),
-        exercise_barnes_hut: resolve(__dirname, 'src/pages/exercise/barnes-hut/index.html'),
-        exercise_fdp: resolve(__dirname, 'src/pages/exercise/fdp/index.html'),
+        main: resolve(__dirname, "index.html"),
+        nested: resolve(__dirname, "src/pages/barnes-hut/index.html"),
+        exercise_barnes_hut: resolve(
+          __dirname,
+          "src/pages/exercise/barnes-hut/index.html"
+        ),
+        exercise_fdp: resolve(
+          __dirname,
+          "src/pages/exercise/fdp_barnes-hut/index.html"
+        ),
       },
     },
   },
-})
+});
